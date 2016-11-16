e60471b8c6cdb88b3a5ebfeaee6d898f9a8506b0
MikhailSobol <mikhailsobolemail@gmail.com>
2016-11-16 18:25:54 +0200

    TicTacTorField changed to IField

--- src/com/mikhailsobol/tictactoe/model/games/MultiplayerTicTacToeGame.java
+++ src/com/mikhailsobol/tictactoe/model/games/MultiplayerTicTacToeGame.java
@@ -2,12 +2,13 @@ package com.mikhailsobol.tictactoe.model.games;
 
 
 import com.mikhailsobol.tictactoe.model.Player;
+import com.mikhailsobol.tictactoe.model.fields.IField;
 import com.mikhailsobol.tictactoe.model.fields.TicTacToeField;
 
 public class MultiplayerTicTacToeGame<Figure> extends AbstractTicTacToeGame<Figure> {
 
     public MultiplayerTicTacToeGame(final Player<Figure>[] players,
-                                    final TicTacToeField<Figure> field,
+                                    final IField<Figure> field,
                                     final boolean isSinglePlayer,
                                     final String name) {
         super(players, field, isSinglePlayer, name);
