263a9938172bab246a1ba013cdff884a0855af36
MikhailSobol <mikhailsobolemail@gmail.com>
2016-11-17 14:51:42 +0200

    countOccupiedCells is public now.

--- src/com/mikhailsobol/tictactoe/controller/TicTacToeCurrentMoveController.java
+++ src/com/mikhailsobol/tictactoe/controller/TicTacToeCurrentMoveController.java
@@ -21,6 +21,18 @@ public class TicTacToeCurrentMoveController {
         return currentPlayer;
     }
 
+    public int countOccupiedCells(final IField field) throws InvalidCoordinateException {
+        int count = 0;
+        for (int i = 0; i < field.getSize(); i++) {
+            for (int j = 0; j < field.getSize(); j++) {
+                if (field.getFigure(new Point(i , j)) != null) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
     private Player getPlayerByFigure(final Figure currentFigure, final Player[] players)
             throws PlayerNotFoundException {
         for (final Player player : players) {
@@ -35,16 +47,5 @@ public class TicTacToeCurrentMoveController {
         return countOfOccupied % 2 == 0 ? Figure.X : Figure.O;
     }
 
-    private int countOccupiedCells(final IField field) throws InvalidCoordinateException {
-        int count = 0;
-        for (int i = 0; i < field.getSize(); i++) {
-            for (int j = 0; j < field.getSize(); j++) {
-                if (field.getFigure(new Point(i , j)) != null) {
-                    count++;
-                }
-            }
-        }
-        return count;
-    }
 
 }
