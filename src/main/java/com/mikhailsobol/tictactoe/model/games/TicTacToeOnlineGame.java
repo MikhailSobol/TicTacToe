1664a74688d87dbbacc5f027f800ced44aee3ecd
Mikhail Sobol <mikhailsobolemail@gmail.com>
2017-01-05 11:42:53 +0200

    TicTacTieOnlineGame model created

---
+++ src/main/java/com/mikhailsobol/tictactoe/model/games/TicTacToeOnlineGame.java
@@ -0,0 +1,22 @@
+package com.mikhailsobol.tictactoe.model.games;
+
+import com.mikhailsobol.tictactoe.model.Player;
+import com.mikhailsobol.tictactoe.model.fields.IField;
+import com.mikhailsobol.tictactoe.server.TicTacToeServer;
+
+public class TicTacToeOnlineGame extends MultiplayerTicTacToeGame {
+
+    private final TicTacToeServer server;
+
+    public TicTacToeOnlineGame(final Player[] players, IField field,
+                               final boolean isSinglePlayer,
+                               final String name,
+                               final TicTacToeServer server) {
+        super(players, field, isSinglePlayer, name);
+        this.server = server;
+    }
+
+    public TicTacToeServer getServer() {
+        return server;
+    }
+}
